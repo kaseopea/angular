6f9863d9f9e5fcbef48a0ab5f32bba7ded15258c
kaseopea <kaseopea@gmail.com>
2016-07-30 13:57:20 +0300

    cleanup

--- angulartask1/app/components/createCard/createCard.controller.js
+++
@@ -1,8 +0,0 @@
-app.controller('createCardCtrl', ['$scope', 'CONST_VALIDATORS', function($scope, CONST_VALIDATORS) {
-    var vm = this;
-
-    //Sending some constants to view
-    vm.nameMaxWords = CONST_VALIDATORS.MAX_WORDS_IN_NAME;
-    vm.minAge = CONST_VALIDATORS.AGE_MINIMUM;
-    vm.maxAge = CONST_VALIDATORS.AGE_MAXIMUM;
-}]);
--- angulartask1/app/components/index/index.controller.js
+++
@@ -1,6 +0,0 @@
-app.controller('indexCtrl', ['$scope', 'CONST_VALIDATORS', function($scope, CONST_VALIDATORS) {
-    //Sending some constants to view
-    this.nameMaxWords = CONST_VALIDATORS.MAX_WORDS_IN_NAME;
-    this.minAge = CONST_VALIDATORS.AGE_MINIMUM;
-    this.maxAge = CONST_VALIDATORS.AGE_MAXIMUM;
-}]);
